2f1bca0737dde2700e478dda42cea735ba1b6c53
Andrew Huth <git@huth.me>
2024-10-13 20:34:06 -0400

    Add eslint-plugin-react-hooks

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import ts from 'typescript-eslint';
 import prettierRecommended from 'eslint-plugin-prettier/recommended';
 import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 import globals from 'globals';
 
 /** @type {import('@types/eslint').Linter.Config} */
@@ -28,6 +29,15 @@ export default [
       react: {
         version: 'detect',
       },
+    },
+  },
+  {
+    files: ['src/**/*.{jsx,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
     },
   },
   prettierRecommended,
